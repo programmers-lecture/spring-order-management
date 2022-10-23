583c3aa458ccb4256804d8d872962911e175dece
seyun(Marco) <ksy90101@gmail.com>
2022-10-23 16:54:38 +0900

    project settings

---
+++ src/main/java/prgms/lecture/order_management/SpringOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package prgms.lecture.order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOrderManagementApplication.class, args);
+    }
+
+}
