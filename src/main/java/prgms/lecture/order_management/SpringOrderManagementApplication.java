b35288767815e9b6b03d55af1826acab632f8e49
Anwooseong <aws5624@naver.com>
2022-10-25 20:28:01 +0900

    test

--- src/main/java/prgms/lecture/order_management/SpringOrderManagementApplication.java
+++ src/main/java/prgms/lecture/order_management/SpringOrderManagementApplication.java
@@ -2,7 +2,7 @@ package prgms.lecture.order_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringOrderManagementApplication {
 
